de836a56808cac214d9dda904fa4f5aa18bd1d9c
Björn Friedrichs <bjoern@friedrichs1.de>
2017-03-28 18:01:34 +0200

    fancier animation

--- static/js/logo.js
+++ static/js/logo.js
@@ -34,17 +34,24 @@ var Bubble = function() {
   this.y = 0;
   this.size = 0;
   this.max_size = 0;
+  this.iterations_left = 5;
 };
 
 var bubbles = [];
-var bubble_max_count = 25;
+var bubble_max_count = 35;
 
-var bubble_min_size = 10;
+var bubble_min_size = 1;
 var bubble_max_size = canvasHeaderContainer.clientHeight / 8;
 
-for(var i = 0; i < bubble_max_count; i++) {
+var interval = setInterval(function() {
   bubbles.push(new Bubble());
-}
+  if(bubbles.length >= bubble_max_count) {
+    clearInterval(interval);
+  }
+}, 100);
+
+// https://gist.github.com/gre/1650294
+function ease(t) { return 1+(--t)*t*t*t*t; }
 
 function addExtras() {
   logo_extra.clear();
@@ -53,17 +60,21 @@ function addExtras() {
   for(var i in bubbles) {
     var bubble = bubbles[i];
 
-    if(bubble.init && bubble.size < bubble.max_size) {
-      bubble.size += Math.random() / 2;
+    if(bubble.init && bubble.size < bubble.max_size - 5) {
+      var curr = bubble.size / bubble.max_size;
+      bubble.size += (bubble.max_size * ease(curr) - bubble.size) / 25;
     } else {
-      var at = Math.random() * 2;
-      var radius = Math.min(0.5 + Math.random(), 1.1) * logo_circle_size;
-      bubble.x = radius * Math.cos(at * Math.PI);
-      bubble.y = radius * Math.sin(at * Math.PI);
-      bubble.size = bubble_min_size;
-      bubble.max_size = bubble_max_size * Math.min(0.5 + Math.random(), 1);
+      if(bubble.iterations_left > 0) {
+        var at = Math.random() * 2;
+        var radius = Math.min(0.5 + Math.random(), 1) * logo_circle_size;
+        bubble.x = radius * Math.cos(at * Math.PI);
+        bubble.y = radius * Math.sin(at * Math.PI);
+        bubble.size = bubble_min_size;
+        bubble.max_size = (bubble_max_size / bubble.iterations_left) * Math.min(0.5 + Math.random(), 1);
+        bubble.iterations_left -= 1;
 
-      bubble.init = true;
+        bubble.init = true;
+      }
     }
 
     logo_extra.drawCircle(bubble.x, bubble.y, bubble.size);
